e1d071eb502516227d9dab9a16b495edf4b4525d
iliana destroyer of worlds <iliana@buttslol.net>
2019-12-01 23:38:06 -0600

    split intcode into its own crate

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,61 +1,19 @@
-use std::io::prelude::*;
+use intcode::intcode;
+use std::io::BufReader;
 
-fn intcode(input: Vec<usize>) -> Vec<usize> {
-    let mut input = input;
-    let mut i = 0;
-    loop {
-        match input[i] {
-            1 => {
-                let a = input[i + 1];
-                let b = input[i + 2];
-                let x = input[i + 3];
-                input[x] = input[a] + input[b];
-                i += 4;
-            }
-            2 => {
-                let a = input[i + 1];
-                let b = input[i + 2];
-                let x = input[i + 3];
-                input[x] = input[a] * input[b];
-                i += 4;
-            }
-            99 => return input,
-            _ => unreachable!(),
-        }
-    }
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut values_str = String::new();
-    std::io::stdin().read_to_string(&mut values_str)?;
-    let values = values_str
-        .trim()
-        .split(',')
-        .map(|s| s.parse())
-        .collect::<Result<Vec<usize>, _>>()?;
+fn main() -> std::io::Result<()> {
+    let program = intcode::load(&mut BufReader::new(std::io::stdin()))?;
     for noun in 0..100 {
         for verb in 0..100 {
-            let mut values = values.clone();
-            values[1] = noun;
-            values[2] = verb;
-            let values = intcode(values);
-            if values[0] == 19690720 {
+            let mut program = program.clone();
+            program[1] = noun;
+            program[2] = verb;
+            intcode(&mut program);
+            if program[0] == 19690720 {
                 println!("{}", 100 * noun + verb);
                 break;
             }
         }
     }
     Ok(())
-}
-
-#[cfg(test)]
-#[test]
-fn test() {
-    assert_eq!(intcode(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
-    assert_eq!(intcode(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
-    assert_eq!(intcode(vec![2, 4, 4, 5, 99, 0]), vec![2, 4, 4, 5, 99, 9801]);
-    assert_eq!(
-        intcode(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
-        vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
-    );
 }
---
+++ intcode/src/lib.rs
@@ -0,0 +1,59 @@
+use std::io::{BufRead, Error, ErrorKind, Result};
+
+pub fn load<R: BufRead>(reader: &mut R) -> Result<Vec<usize>> {
+    reader
+        .split(b',')
+        .map(|b| {
+            b.and_then(|b| {
+                String::from_utf8(b)
+                    .map_err(|e| Error::new(ErrorKind::InvalidData, e))
+                    .and_then(|s| {
+                        s.trim()
+                            .parse()
+                            .map_err(|e| Error::new(ErrorKind::InvalidData, e))
+                    })
+            })
+        })
+        .collect()
+}
+
+pub fn intcode(input: &mut [usize]) -> &mut [usize] {
+    let mut ip = 0;
+    loop {
+        match input[ip] {
+            1 => {
+                // add
+                let a = input[ip + 1];
+                let b = input[ip + 2];
+                let x = input[ip + 3];
+                input[x] = input[a] + input[b];
+                ip += 4;
+            }
+            2 => {
+                // multiply
+                let a = input[ip + 1];
+                let b = input[ip + 2];
+                let x = input[ip + 3];
+                input[x] = input[a] * input[b];
+                ip += 4;
+            }
+            99 => {
+                // halt
+                break input;
+            }
+            _ => unimplemented!(),
+        }
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test() {
+    assert_eq!(intcode(&mut [1, 0, 0, 0, 99]), &[2, 0, 0, 0, 99]);
+    assert_eq!(intcode(&mut [2, 3, 0, 3, 99]), &[2, 3, 0, 6, 99]);
+    assert_eq!(intcode(&mut [2, 4, 4, 5, 99, 0]), &[2, 4, 4, 5, 99, 9801]);
+    assert_eq!(
+        intcode(&mut [1, 1, 1, 4, 99, 5, 6, 0, 99]),
+        &[30, 1, 1, 4, 2, 5, 6, 0, 99]
+    );
+}
