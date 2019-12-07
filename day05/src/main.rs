7a4faf1efa6502fd8a979375c9b91d53452b3913
iliana destroyer of worlds <iliana@buttslol.net>
2019-12-07 00:32:30 -0800

    day 5 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -2,9 +2,20 @@ use intcode::intcode;
 use std::io::BufReader;
 
 fn main() -> std::io::Result<()> {
-    let mut program = intcode::load(&mut BufReader::new(std::io::stdin()))?;
-    for output in intcode(&mut program, vec![1]) {
-        println!("{}", output);
+    let program = intcode::load(&mut BufReader::new(std::io::stdin()))?;
+    {
+        println!("day 1:");
+        let mut program = program.clone();
+        for output in intcode(&mut program, vec![1]) {
+            println!("{}", output);
+        }
+    }
+    {
+        println!("day 2:");
+        let mut program = program.clone();
+        for output in intcode(&mut program, vec![5]) {
+            println!("{}", output);
+        }
     }
     Ok(())
 }
