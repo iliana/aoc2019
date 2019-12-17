5da66ffefbd5d91cf3f686b31df18cb9e57c008e
iliana destroyer of worlds <iliana@buttslol.net>
2019-12-16 23:52:50 -0800

    day 17

---
+++ day17/src/main.rs
@@ -0,0 +1,90 @@
+use intcode::Runner;
+use std::fmt;
+use std::ops::Index;
+use std::task::Poll;
+
+#[derive(Debug)]
+struct Map(String);
+
+impl Map {
+    fn read(runner: &mut Runner) -> Map {
+        let mut s = String::new();
+        while let Some(Poll::Ready(n)) = runner.next() {
+            s.push(char::from(n as u8));
+        }
+        Map(s)
+    }
+
+    fn width(&self) -> usize {
+        self.0.lines().next().unwrap().len()
+    }
+
+    fn height(&self) -> usize {
+        self.0.trim().lines().count()
+    }
+
+    fn coord_to_idx(&self, coord: (usize, usize)) -> usize {
+        let w = self.width() + 1;
+        coord.1 * w + coord.0
+    }
+}
+
+impl fmt::Display for Map {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0.trim())
+    }
+}
+
+impl Index<(usize, usize)> for Map {
+    type Output = str;
+
+    fn index(&self, coord: (usize, usize)) -> &str {
+        let idx = self.coord_to_idx(coord);
+        &self.0[idx..=idx]
+    }
+}
+
+fn main() {
+    let input = util::read_intcode();
+
+    let mut program = [0; 8192];
+    program[..input.len()].copy_from_slice(&input);
+    let mut runner = Runner::new(&mut program);
+    let map = Map::read(&mut runner);
+    println!("{}", map);
+    let mut sum = 0;
+    for x in 1..(map.width() - 1) {
+        for y in 1..(map.height() - 1) {
+            if &map[(x, y)] == "#"
+                && &map[(x - 1, y)] == "#"
+                && &map[(x + 1, y)] == "#"
+                && &map[(x, y - 1)] == "#"
+                && &map[(x, y + 1)] == "#"
+            {
+                sum += x * y;
+            }
+        }
+    }
+    println!("part 1: {}", sum);
+
+    // it'd be neat to figure this out programatically but my brain works just as well!
+    let a = "L,8,R,10,L,10";
+    let b = "R,10,L,8,L,8,L,10";
+    let c = "L,4,L,6,L,8,L,8";
+    let main = "A,B,A,C,B,C,A,C,B,C";
+
+    let mut stdin = format!("{}\n{}\n{}\n{}\nn\n", main, a, b, c);
+
+    let mut program = [0; 8192];
+    program[..input.len()].copy_from_slice(&input);
+    program[0] = 2;
+    let mut runner = Runner::new(&mut program);
+    let mut last = 0;
+    while let Some(poll) = runner.next() {
+        match poll {
+            Poll::Ready(v) => last = v,
+            Poll::Pending => runner.input(u32::from(stdin.remove(0)).into()),
+        }
+    }
+    println!("part 2: {}", last);
+}
